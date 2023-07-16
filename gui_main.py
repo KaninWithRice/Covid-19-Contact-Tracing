36a540ee5b545ba68ad2e82e3f8ba5247bbddd88
KaninWithRice <mishalnorona014@gmail.com>
2023-07-16 23:50:35 +0800

    First name entry

--- gui_main.py
+++ gui_main.py
@@ -8,7 +8,9 @@ gui.title ("Covid-19 Tracing")
 # Add window size
 gui.geometry ("600x400")
 # Create a pack widgets
-pad = tkinter.Frame(gui)
-pad.pack()
-
+pad1 = tkinter.Frame(gui)
+pad1.pack()
+# Add Entries
+fname_entry = tkinter.Entry(pad1)
+fname_entry.grid(row=1, column=0)
 gui.mainloop()
