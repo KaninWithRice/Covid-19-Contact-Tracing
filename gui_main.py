21dffc719d73b11bab5b20f08d683839662efba9
KaninWithRice <mishalnorona014@gmail.com>
2023-07-16 23:45:16 +0800

    Windows Size and Pack

--- gui_main.py
+++ gui_main.py
@@ -1,8 +1,14 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Contact Tracing
 # Import tkinter
 import tkinter
+
 # Create a gui window
 gui = tkinter.Tk()
 gui.title ("Covid-19 Tracing")
+# Add window size
+gui.geometry ("600x400")
+# Create a pack widgets
+pad = tkinter.Frame(gui)
+pad.pack()
 
 gui.mainloop()
