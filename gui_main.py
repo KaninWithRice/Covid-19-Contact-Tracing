66aa10f92287c85430e8afb0abc1f24ffac3c45e
KaninWithRice <mishalnorona014@gmail.com>
2023-07-17 00:18:14 +0800

    Done Entry Row 1

--- gui_main.py
+++ gui_main.py
@@ -1,7 +1,7 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Contact Tracing
 # Import tkinter
 import tkinter
-
+from tkinter import ttk
 # Create a gui window
 gui = tkinter.Tk()
 gui.title ("Covid-19 Tracing")
@@ -13,4 +13,13 @@ pad1.pack()
 # Add Entries
 fname_entry = tkinter.Entry(pad1)
 fname_entry.grid(row=1, column=0)
+
+mname_entry = tkinter.Entry(pad1)
+mname_entry.grid(row=1, column=1)
+
+lname_entry = tkinter.Entry(pad1)
+lname_entry.grid(row=1, column=2)
+
+suffix_entry = ttk.Combobox(pad1, values=["", "Jr.", "Sr.", "II", "III", "IV", "V"])
+suffix_entry.grid(row=1, column=3)
 gui.mainloop()
