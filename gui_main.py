0ac79684e07e051e2971a23658da925f41740134
KaninWithRice <mishalnorona014@gmail.com>
2023-07-16 20:11:51 +0800

    window created

--- gui_main.py
+++ gui_main.py
@@ -1,3 +1,8 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Contact Tracing
 # Import tkinter
+import tkinter
 # Create a gui window
+gui = tkinter.Tk()
+gui.title ("Covid-19 Tracing")
+
+gui.mainloop()
