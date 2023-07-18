291f2578844fa7d739bea3d27ef314fb1640daa5
KaninWithRice <mishalnorona014@gmail.com>
2023-07-19 02:50:14 +0800

    Psuedocode of Main Menu

---
+++ Main_Menu.py
@@ -0,0 +1,8 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Contact Tracing
+# Import modules
+# import class
+# Create a Class for Main Menu
+# Create a Frame
+# Add button
+# Call class and run
+# Run the Menu Class
