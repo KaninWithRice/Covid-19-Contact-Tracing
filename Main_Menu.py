34fe1070973aa31ef8e36d646d9a6f446d0a41a2
KaninWithRice <mishalnorona014@gmail.com>
2023-07-19 02:55:45 +0800

    Import Tkinter and Register Class

--- Main_Menu.py
+++ Main_Menu.py
@@ -1,6 +1,9 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Contact Tracing
 # Import modules
+import tkinter as tk
+from tkinter import messagebox
 # import class
+from Register_Info_Class import register_info
 # Create a Class for Main Menu
 # Create a Frame
 # Add button
