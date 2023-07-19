0da055fab4e9684dc3f8cef2ba5eaf9f55730756
KaninWithRice <mishalnorona014@gmail.com>
2023-07-20 00:37:19 +0800

    Revising Search Engine Psuedocode

--- Search_Engine_Class.py
+++ Search_Engine_Class.py
@@ -88,62 +88,11 @@ class search_info:
 
     # Search Function
     def search_engine(self):
-        first_name = self.fname_entry.get()
-        middle_name =self.mname_entry.get()
-        last_name = self.lname_entry.get()
-        suffix = self.suffix_entry.get()
-        housenum = self.housenum_entry.get()
-        street = self.street_entry.get()
-        bgry = self.brgy_entry.get()
-        city = self.city_entry.get()
-        age = self.age_entry.get()
-        contactnum = self.contactnum_entry.get()
-        email = self.email_entry.get()
-        gender = self.gender_entry.get()
-        # Check if at least one field is filled
-        if not any([first_name, middle_name, last_name, suffix, housenum, street, bgry, city, age, contactnum, email, gender]):
-            messagebox.showerror("ERROR: PLEASE ENTER AT LEAST ONE INFORMATION")# Add Error Input 
-            return
-        
-        data_found = []     #Added object for data entries found
-        # Read Data
-        with open("Data_List.csv", "r") as file:
-            reader = csv.reader(file)
-            header = next(reader)  # Get the header row
-        # Allow user to search from any Personal Information
-            for row in reader:
-                match = True
-
-                # Check each search criteria against the corresponding field
-                if first_name and row[0] != first_name:
-                    match = False
-                if middle_name and row[1] != middle_name:
-                    match = False
-                if last_name and row[2] != last_name:
-                    match = False
-                if suffix and row[3] != suffix:
-                    match = False
-                if housenum and row[4] != housenum:
-                    match = False
-                if street and row[5] != street:
-                    match = False
-                if bgry and row[6] != bgry:
-                    match = False
-                if city and row[7] != city:
-                    match = False
-                if age and row[8] != age:
-                    match = False
-                if contactnum and row[9] != contactnum:
-                    match = False
-                if email and row[10] != email:
-                    match = False  
-                if gender and row[11] != gender:
-                    match = False                  
-                if match:
-                    data_found.append(row)
-        # Show result
-        if data_found:
-            results = "\n".join([f"\n Name: {entry[0]} {entry[1]} {entry[3]} {entry[3]} \n Age: {entry[8]} \n Gender: {entry[11]} \n Address: {entry[4]} {entry[5]} {entry[6]} {entry[7]}, \n Email: {entry[2]}" for entry in data_found])
-            messagebox.showinfo("Search Results: ", f"FOUND {len(data_found)} ENTRIES MATCH WITH THE DATA:\n\n{results}")
-        else:
-            messagebox.showinfo("Search Results: ", "NO ENTRIES FOUND")
+    # Create an Object for the variables
+    # Determine the search value
+    # Check if search value is entered
+    # Create a Temp Storage
+    # Read csv file
+    # Check each criteria 
+    # Show Result
+    # Add message box if entry not found or found
